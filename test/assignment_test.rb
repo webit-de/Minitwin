ad04778f3760442776f0314b48fd709ed7e39148
Johannes Balk <balk@webit.de>
2026-01-22 11:23:31 +0100

    [TASK] Add another boolean test

--- test/assignment_test.rb
+++ test/assignment_test.rb
@@ -4,6 +4,7 @@ require "mini_twin"
 class AssignSubTwin < MiniTwin
   property :sub_property
   property :another_sub_property, default: "default"
+  property :bool?, type: Types::Params::Bool.lax
 end
 
 class AssignmentTest < ActiveSupport::TestCase
@@ -21,5 +22,15 @@ class AssignmentTest < ActiveSupport::TestCase
     assert_equal "test", twin.sub_property
     assert_equal "not default", twin.another_sub_property
   end
+
+  test "should assign boolean values" do
+    twin = AssignSubTwin.new(**{ "bool?" => true })
+    assert_instance_of TrueClass, twin.bool?
+    assert twin.bool?
+    twin = AssignSubTwin.new(**{ "bool?" => false })
+    assert_instance_of FalseClass, twin.bool?
+    assert_not twin.bool?
+  end
+
 end
 
