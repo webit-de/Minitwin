b5df250f289fecdef552c8bb5ae675a77f18892b
Roland Schwarzer <schwarzer@webit.de>
2026-05-20 13:58:57 +0200

    [MAINTENANCE] fix load path of rake task in Railtie

--- lib/mini_twin/railtie.rb
+++ lib/mini_twin/railtie.rb
@@ -1,7 +1,7 @@
 class Minitwin
   class Railtie < Rails::Railtie
     rake_tasks do
-      load File.join(__dir__, "../../tasks/mini_twin.rake")
+      load File.join(__dir__, "..", "tasks", "mini_twin.rake")
     end
   end
 end
